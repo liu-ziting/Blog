d6f43671376a20915231f8c18f69d499c5a43604
liu-ziting <liuziting2014@163.com>
2019-11-21 15:52:04 +0800

    blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,126 @@
+module.exports = {
+		base: '',// 设置站点根路径
+    title: 'Liuziting', // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
+    description: '刘子亭的个人博客', // meta 中的描述文字，用于SEO
+    // 注入到当前页面的 HTML <head> 中的标签
+    head: [
+        ['link', { rel: 'icon', href: '/xiao.png' }],  //浏览器的标签栏的网页图标
+    ],
+    markdown: {
+        lineNumbers: true
+    },
+    serviceWorker: true,
+    themeConfig: {    
+        logo: '/xiao.png',
+        lastUpdated: 'lastUpdate', // string | boolean
+        nav: [
+            { text: '首页', link: '/' },
+            {
+                text: '分类',
+                ariaLabel: '分类',
+                items: [
+                    { text: '文章', link: '/pages/article/frontend/Adminair.md' },
+                    { text: '软件', link: '/pages/software/index.md' },
+										{ text: '项目', link: '/pages/project.md' },
+                ]
+            },
+						{ text: '关于我', link: '/pages/about.md' },
+						{ text: '图传工具', link: 'https://bat.lihail.cn' },
+						{ text: 'Wiki', link: 'https://wiki.lihail.cn' },
+            { text: 'Github', link: 'https://github.com/liu-ziting' },
+        ],
+        sidebar: {
+            '/pages/article/':[
+                {
+                    title: '前端开发',   // 必要的
+                    collapsable: false, // 可选的, 默认值是 true,
+                    sidebarDepth: 1,    // 可选的, 默认值是 1
+                    children: [
+												['frontend/Adminair.md', 'Vue+El构建的后台管理系统'],
+												['frontend/Wiki.md', '基于GitBook搭建的知识库管理系统'],
+												['frontend/Vue.md', 'Vue相关开源项目库汇总'],
+                        ['frontend/Jrx.md', '前端兼容性问题总结'],
+												['frontend/Js.md', '常用js方法整理'],
+												['frontend/Fx.md', '移动端键盘遮住输入框问题'],
+												['frontend/Bsx.md', 'ios返回后页面不刷新的问题'],
+												['frontend/Jz.md', '多种方法实现水平居中垂直居中'],
+												['frontend/Html.md', '你不知道的HTML'],
+												['frontend/Ajax.md', 'ajax长轮询、轮询应用和介绍'],
+												
+                    ]
+                },
+                {
+                    title: '博客搭建',
+                    collapsable: false, // 可选的, 默认值是 true,
+										sidebarDepth: 1,    // 可选的, 默认值是 1
+                    children: [
+                        ['blog/Hexo.md', '最快的 Hexo 博客搭建方法'],
+												['blog/Wiki.md', 'Wiki 知识库管理搭建']
+                    ]
+                },
+								{
+								    title: '开发流程',
+								    collapsable: false, // 可选的, 默认值是 true,
+										sidebarDepth: 1,    // 可选的, 默认值是 1
+								    children: [
+								        ['blog/Wx.md', '微信公众号开发流程'],
+												['blog/Dd.md', '钉钉微应用接入']
+								    ]
+								},
+								{
+								    title: 'Python',
+								    collapsable: false, // 可选的, 默认值是 true,
+										sidebarDepth: 1,    // 可选的, 默认值是 1
+								    children: [
+								        ['python/Python.md', '微信防撤回的监听'],
+								    ]
+								},
+								{
+								    title: '工具',
+								    collapsable: false, // 可选的, 默认值是 true,
+										sidebarDepth: 1,    // 可选的, 默认值是 1
+								    children: [
+												['tool/Natapp.md', '内网穿透工具natapp'],
+												['tool/Eruda.md', '移动端调试神器eruda'],
+												['tool/Vconsole.md', '移动端调试神器vconsole'],
+								    ]
+								},
+								{
+								    title: '其他',
+								    collapsable: false, // 可选的, 默认值是 true,
+										sidebarDepth: 1,    // 可选的, 默认值是 1
+								    children: [
+												['other/Tt.md', '头条是如何挖掘多级朋友圈关系链的'],
+												['other/Hyjs.md', '微信群里各种「好友加速」真的可以加速成功吗？'],
+								        ['other/Jj.md', '基金新手入门必看知识点'],
+												['other/Kf.md', 'Facebook 上一位大佬发表的代码看法'],
+												['other/Qkl.md', '浅谈区块链技术'],
+												['other/Bn.md', 'JavaScript和地球变暖的关系'],
+								    ]
+								},
+								{
+								    title: '琐碎',
+								    collapsable: false, // 可选的, 默认值是 true,
+										sidebarDepth: 1,    // 可选的, 默认值是 1
+								    children: [
+												['life/Pz.md', '你比我想的更偏执'],
+												['life/Sd.md', '你们觉得这个时代好还是父母那个时代好？'],
+												['life/Xh.md', '喜欢是多么不靠谱的事情'],
+												['life/Dd.md', '滴血的滴滴：你想知道的，正是他不愿说的'],
+												['life/Jk.md', '从这件事情为什么发生在我身上到这件事情教会我什么'],
+												
+								    ]
+								},
+								{
+								    title: '总结',
+								    collapsable: false, // 可选的, 默认值是 true,
+										sidebarDepth: 1,    // 可选的, 默认值是 1
+								    children: [
+												['life/2018.md', '2018年度总结'],
+												
+								    ]
+								},
+            ],
+        }
+    }
+}
---
+++ docs/pages/software/index.md
@@ -0,0 +1,27 @@
+## 软件下载
+
+>关于破解软件、翻墙工具、开发工具等等常用软件的下载地址集合,有好玩的软件或破解版会持续更新中...
+
+
+-----
+
+| 软件     | 下载地址 | 提取码 |
+| --------| -----|-----|
+|破解版百度云（PanDownload） | [下载地址][1] |r9xh  |
+|谷歌翻墙工具| [下载地址][2]| m3pe |
+|Adobe Premiere Pro CS6 绿色破解版 | [下载地址][3] |i6ha  |
+|Adobe Premiere Pro 2017-11.0（含破解补丁）|[下载地址][4] |o8gg|
+|万能驱动 |[下载地址][5]| p0ro|
+|电脑微信2.6.7.57-防撤回版|[下载地址][6]| szns|
+
+#### 更多软件下载戳这里：[wiki][99]
+
+[1]: https://pan.baidu.com/s/1xMu-6AtPKTrEy4qT8qsaNg
+[2]: https://pan.baidu.com/s/1DQNjF8PxRdRVdoq7Pq5r1g
+[3]: https://pan.baidu.com/s/1tQr-qUDqhYSGmNzGIpXohA
+[4]: https://pan.baidu.com/s/1A5MB2dBBMYB_9wLd64Oidg
+[5]: https://pan.baidu.com/s/1U-RLkj603tgOri8xOt7EUA
+[6]: https://pan.baidu.com/s/1wdZ3TI77kK1h_8u7CpmqtQ
+[99]: http://wiki.lihail.cn/page/software/1.html
+	
+
